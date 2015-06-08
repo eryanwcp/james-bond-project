8f0e299de308ebc08c0ea311bd9cd7333113695e
Serkan.Tugrul <serkan.tugrul@sagepay.com>
2015-06-08 17:59:14 +0100

    Tidied up the table and made it look a bit nicer

---
+++ src/main/java/com/coffeeshop/coffee/Channel.java
@@ -0,0 +1,15 @@
+package com.coffeeshop.coffee;
+
+/**
+ * Created by Serkan.Tugrul on 08/06/2015.
+ */
+public class Channel {
+    private String nine;
+    private String ten;
+    private String eleven;
+    private String twelve;
+    private String thirteen;
+    private String fourteen;
+    private String fifteen;
+    private String sixteen;
+}
--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -11,32 +11,97 @@ coffeeApp.factory('CoffeeOrder', function ($resource) {
 
 
 coffeeApp.controller('OrderController', function ($scope, CoffeeOrder, $http) {
-    $scope.types = [
-        {name: 'Americano', family: 'Coffee'},
-        {name: 'Latte', family: 'Coffee'},
-        {name: 'Tea', family: 'that other drink'},
-        {name: 'Cappuccino', family: 'Coffee'}
-    ];
+    //$scope.types = [
+    //    {name: 'Americano', family: 'Coffee'},
+    //    {name: 'Latte', family: 'Coffee'},
+    //    {name: 'Tea', family: 'that other drink'},
+    //    {name: 'Cappuccino', family: 'Coffee'}
+    //];
+
 
     $scope.channels = [
-        {name: 'Sean Connery', programme: 'Dr. No'},
-        {name: 'George Lazenby', programme: 'Goldfinger'},
-        {name: 'Timothy Dalton', programme: 'Moonraker'}
-    ];
 
-    $http.get('application/coffeeshop/nearest/Finchley')
-        .success(function(response) {$scope.coffeeshops = response.coffeeshops;});
+        {
+            name: 'Sean Connery',
+            nineAm: 'Goldfinger',
+            tenAm: 'Goldfinger',
+            elevenAm: 'No programmes available',
+            twelvePm: 'Moonraker',
+            onePm: 'Moonraker',
+            twoPm: 'No programmes available',
+            threePm: 'Dr. No'
+        },
+        {
+            name: 'George Lazenby',
+            nineAm: 'No programmes available',
+            tenAm: 'Skyfall',
+            elevenAm: 'Skyfall',
+            twelvePm: 'No programmes available',
+            onePm: 'No programmes available',
+            twoPm: 'Licence to Kill',
+            threePm: 'Licence to Kill'
+        },
+        {
+            name: 'Roger Moore',
+            nineAm: 'Thunderball',
+            tenAm: 'Thunderball',
+            elevenAm: 'No programmes available',
+            twelvePm: 'Die Another Day',
+            onePm: 'Die Another Day',
+            twoPm: 'No programmes available',
+            threePm: 'No programmes available'
+        },
+        {
+            name: 'Timothy Dalton',
+            nineAm: "On Her Majesty's Secret Service",
+            tenAm: "On Her Majesty's Secret Service",
+            elevenAm: "No programmes available",
+            twelvePm: "No programmes available",
+            onePm: 'Casino Royal',
+            twoPm: 'Casino Royal',
+            threePm: 'Casino Royal'
 
-    $scope.sizes = ['Small', 'Medium', 'Large'];
-    $scope.messages = [];
-    $scope.regions = ['Finchley', 'Islington', 'Barnet', 'Croydon'];
+        },
+        {
+            name: 'Pierce Brosnan',
+            nineAm: 'No programmes available',
+            tenAm: 'No programmes available',
+            elevenAm: 'No programmes available',
+            twelvePm: 'A View to Kill',
+            onePm: 'A View to Kill',
+            twoPm: 'No programmes available',
+            threePm: 'No programmes available'
+        },
+        {
+            name: 'Daniel Craig',
+            nineAm: 'Quantum of Solace',
+            tenAm: 'Quantum of Solace',
+            elevenAm: 'No programmes available',
+            twelvePm: 'No programmes available',
+            onePm: 'You Only Live Twice',
+            twoPm: 'You Only Live Twice',
+            threePm: 'No programmes available'
+        }
 
-    $scope.giveMeCoffee = function () {
-        CoffeeOrder.save({id: 1}, $scope.drink, function (order) {
-                $scope.messages.push({type: 'success', msg: 'Order sent!'})
-            }
-        )
-    }
+    ]
+
+
+    //
+    //$http.get('application/coffeeshop/nearest/Finchley')
+    //    .success(function (response) {
+    //        $scope.coffeeshops = response.coffeeshops;
+    //    });
+
+    //$scope.sizes = ['Small', 'Medium', 'Large'];
+    //$scope.messages = [];
+    //$scope.regions = ['Finchley', 'Islington', 'Barnet', 'Croydon'];
+
+    //$scope.giveMeCoffee = function () {
+    //    CoffeeOrder.save({id: 1}, $scope.drink, function (order) {
+    //            $scope.messages.push({type: 'success', msg: 'Order sent!'})
+    //        }
+    //    )
+    //}
 
     $scope.closeAlert = function (index) {
         $scope.messages.splice(index, 1);
